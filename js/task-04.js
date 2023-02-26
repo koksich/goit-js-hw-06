a27db63188e0ac89c03e86d46fb981363558a7fc
Natasha Kuksa <natasha.kuksa.96@gmail.com>
2023-02-26 11:27:30 +0100

    task-04

--- js/task-04.js
+++ js/task-04.js
@@ -9,3 +9,29 @@
 // Создай переменную counterValue в которой будет храниться текущее значение счетчика и инициализируй её значением 0.
 // Добавь слушатели кликов на кнопки, внутри которых увеличивай или уменьшай значение счтетчика.
 // Обновляй интерфейс новым значением переменной counterValue.
+
+
+const counterValue = document.querySelector("#value");
+const decrementBtn = document.querySelector('[data-action="decrement"]');
+const incrementBtn = document.querySelector('[data-action="increment"]');
+
+
+let value = 0;
+
+
+const onDecrementBtnClick = () => {
+    value -= 1;
+    counterValue.textContent = value;
+};
+
+const onIncrementBtnClick = () => {
+    value += 1;
+    counterValue.textContent = value;
+};
+
+
+
+decrementBtn.addEventListener('click', onDecrementBtnClick);
+incrementBtn.addEventListener('click', onIncrementBtnClick);
+
+
